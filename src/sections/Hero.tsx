7ceebc6acc600516a20ee61afab34f64d1006a90
Viktor Rura <ruravictor@gmail.com>
2024-07-24 14:16:13 +0100

    Add cylinde to the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 import ArrowRight from '../assets/arrow-right.svg'
 import cogImage from '../assets/cog.png'
+import cylinder from '../assets/cylinder.png'
 
 export const Hero = () => {
 	return (
-		<section className='pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom__left,#183EC2,#EAEEFE_66%)]'>
+		<section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom__left,#183EC2,#EAEEFE_66%)]'>
 			<div className='container'>
 				<div className='md:flex items-center'>
 					<div className='md:w-[478px]'>
 						<div className='text-sm inline-flex border border-[#222}/10 px-3 py-1 rounded-lg tracking-tight mt-6'>
 							Version 2.0 is here
 						</div>
-						<h1 className='text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text'>
+						<h1 className='text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text'>
 							Pathway to productivity
 						</h1>
 						<p className='text-xl text-[#010d3e] tracking-tight mt-6'>
@@ -33,7 +34,14 @@ export const Hero = () => {
 						<Image
 							src={cogImage}
 							alt='Cog'
-							className='md:absolute md:h-full md:w-auto md:max-w-none'
+							className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6'
+						/>
+						<Image
+							src={cylinder}
+							alt='cylinder'
+							width={220}
+							height={220}
+							className='hidden md:block -top-8 -left-32 md:absolute'
 						/>
 					</div>
 				</div>
