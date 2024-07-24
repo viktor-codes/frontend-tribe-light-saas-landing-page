718a924aa71a5620f2bfe01d5095fc54b8b29480
Viktor Rura <ruravictor@gmail.com>
2024-07-24 16:21:24 +0100

    Hide images for mobile view

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,22 +1,43 @@
 import Image from 'next/image'
 import productImage from '../assets/product-image.png'
+import pyramidImage from '../assets/pyramid.png'
+import tubeImage from '../assets/tube.png'
+
 export const ProductShowcase = () => {
 	return (
-		<section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 '>
+		<section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
 			<div className='container'>
-				<div className='flex-2 justify-center'>
-					<div className='tag'>Boost Your productivity</div>
+				<div className='max-w-[540px] mx-auto'>
+					<div className='flex-2 justify-center'>
+						<div className='tag'>Boost Your productivity</div>
+					</div>
+					<h2 className='text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5'>
+						A more effective way to track progress
+					</h2>
+					<p className='text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5'>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet
+						reprehenderit inventore blanditiis libero asperiores. Ex, pariatur
+						fuga quidem itaque accusantium sit facere autem necessitatibus
+						dolorum veniam, eius hic aliquid.
+					</p>
 				</div>
-				<h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text'>
-					A more effective way to track progress
-				</h2>
-				<p className='text-center'>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet
-					reprehenderit inventore blanditiis libero asperiores. Ex, pariatur
-					fuga quidem itaque accusantium sit facere autem necessitatibus dolorum
-					veniam, eius hic aliquid.
-				</p>
-				<Image src={productImage} alt='Product Image' className='' />
+				<div className='relative'>
+					<Image src={productImage} alt='Product Image' className='mt-10' />
+					<Image
+						src={pyramidImage}
+						alt='Pyramid Image'
+						height={262}
+						width={262}
+						className='hidden md:block absolute -top-32 -right-36'
+					/>
+					<Image
+						src={tubeImage}
+						alt='Tube Image'
+						height={248}
+						width={248}
+						className='hidden md:block absolute bottom-24 -left-36'
+					/>
+				</div>
 			</div>
 		</section>
 	)
