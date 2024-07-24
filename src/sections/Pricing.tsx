98236f12ee171f6cf5f459853335105ce39afc9e
Viktor Rura <ruravictor@gmail.com>
2024-07-24 18:04:03 +0100

    Finish styling pricing cards

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { twMerge } from 'tailwind-merge'
 import CheckIcon from '../assets/check.svg'
 
 const pricingTiers = [
@@ -76,15 +77,27 @@ export const Pricing = () => {
 						}) => (
 							<div
 								key={title}
-								className='p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA]'
+								className={twMerge(
+									'p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA]',
+									inverse === true && 'border-black bg-black text-white'
+								)}
 							>
 								<div className='flex justify-between'>
-									<h3 className='text-lg font-bold text-black/50'>{title}</h3>
-									<div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
-										<span className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)]'>
-											Popular
-										</span>
-									</div>
+									<h3
+										className={twMerge(
+											'text-lg font-bold text-black/50',
+											inverse === true && 'text-white/60'
+										)}
+									>
+										{title}
+									</h3>
+									{popular === true && (
+										<div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
+											<span className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium'>
+												Popular
+											</span>
+										</div>
+									)}
 								</div>
 								<div className='flex items-baseline gap-1 mt-[30px]'>
 									<span className='text-4xl font-bold tracking-tighter leading-none'>
@@ -94,7 +107,12 @@ export const Pricing = () => {
 										/month
 									</span>
 								</div>
-								<button className='btn btn-primary w-full mt-[30px]'>
+								<button
+									className={twMerge(
+										'btn btn-primary w-full mt-[30px]',
+										inverse === true && 'text-black bg-white'
+									)}
+								>
 									{buttonText}
 								</button>
 								<ul className='flex flex-col gap-5 mt-8'>
