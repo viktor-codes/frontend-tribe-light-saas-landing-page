f192b05f81e459a95be00557913809c625a87d4a
Viktor Rura <ruravictor@gmail.com>
2024-07-24 12:54:12 +0100

    Add styles to the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,19 +3,23 @@ export const Hero = () => {
 		<section>
 			<div className='container'>
 				<div>
-					<div className='text-sm inline-flex border border-[#222}/10 px-3 py-1 rounded-lg tracking-tight'>
+					<div className='text-sm inline-flex border border-[#222}/10 px-3 py-1 rounded-lg tracking-tight mt-6'>
 						Version 2.0 is here
 					</div>
-					<h1>Pathway to productivity</h1>
-					<p>
+					<h1 className='text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text'>
+						Pathway to productivity
+					</h1>
+					<p className='text-xl text-[#010d3e] tracking-tight mt-6'>
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis nam
 						ea explicabo. Exercitationem, natus quasi aperiam mollitia facilis
 						sed repudiandae omnis quae beatae quo eligendi laboriosam repellat
 						ut quod optio?
 					</p>
 				</div>
-				<button>Get for free</button>
-				<button>Learn more</button>
+				<div className='flex items-center gap-1 mt-[30px]'>
+					<button>Get for free</button>
+					<button>Learn more</button>
+				</div>
 			</div>
 		</section>
 	)
