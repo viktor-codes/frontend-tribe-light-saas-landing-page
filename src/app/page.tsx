ce0b5d42ca6e057989b80c73b07b35d6a29b5561
Viktor Rura <ruravictor@gmail.com>
2024-07-24 15:33:52 +0100

    Add Product section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { LogoTicker } from '@/sections/LogoTicker'
 import { Header } from '../sections/Header'
 import { Hero } from '../sections/Hero'
+import { ProductShowcase } from '../sections/ProductShowcase'
+
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<Hero />
 			<LogoTicker />
+			<ProductShowcase />
 		</>
 	)
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -9,7 +9,7 @@ import quantumLogo from '../assets/logo-quantum.png'
 
 export const LogoTicker = () => {
 	return (
-		<div className='py-8 bg-white'>
+		<div className='py-8 md:py-12 bg-white'>
 			<div className='container'>
 				<div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
 					<div className='flex gap-14 flex-none'>
