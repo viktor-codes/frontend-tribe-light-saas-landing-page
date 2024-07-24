19dbd7b240b787aac0c513bffcc8b7f0e4a902d2
Viktor Rura <ruravictor@gmail.com>
2024-07-24 15:49:22 +0100

    Add styles to product section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -4,9 +4,13 @@ export const ProductShowcase = () => {
 	return (
 		<section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 '>
 			<div className='container'>
-				<div className='tag'>Boost Your productivity</div>
-				<h2>A more effective way to track progress</h2>
-				<p>
+				<div className='flex-2 justify-center'>
+					<div className='tag'>Boost Your productivity</div>
+				</div>
+				<h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text'>
+					A more effective way to track progress
+				</h2>
+				<p className='text-center'>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet
 					reprehenderit inventore blanditiis libero asperiores. Ex, pariatur
 					fuga quidem itaque accusantium sit facere autem necessitatibus dolorum
