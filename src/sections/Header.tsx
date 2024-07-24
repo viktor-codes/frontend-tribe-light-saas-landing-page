6c85522857dae95f057b20b2425fc7124b14cfad
Viktor Rura <ruravictor@gmail.com>
2024-07-24 12:30:09 +0100

    Add responsiveness to the navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -17,6 +17,16 @@ export const Header = () => {
 					<div className='flex items-center justify-between'>
 						<Image src={Logo} alt='Saas Logo' height={40} width={40} />
 						<MenuIcon className='w-5 h-5 md:hidden' />
+						<nav className='hidden md:flex gap-6 text-black/60 items-center'>
+							<a href=''>About</a>
+							<a href=''>Features</a>
+							<a href=''>Costumers</a>
+							<a href=''>Updates</a>
+							<a href=''>Help</a>
+							<button className='bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex justify-center tracking-tight'>
+								Get for free
+							</button>
+						</nav>
 					</div>
 				</div>
 			</div>
