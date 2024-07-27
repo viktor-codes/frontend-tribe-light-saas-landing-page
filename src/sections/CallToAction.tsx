c34d1f80fcdf57b7d525aa1564aaf00bb35791d9
Viktor Rura <ruravictor@gmail.com>
2024-07-27 11:04:01 +0100

    Add content to the section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,17 @@
 export const CallToAction = () => {
-	return 'call to'
+	return (
+		<section>
+			<div className='container'>
+				<h2>Sign up fo free today</h2>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla rerum,
+					placeat esse architecto exercitationem voluptas labore debitis harum
+					accusamus minima suscipit magnam, atque iure! Tempore esse ut impedit
+					nostrum obcaecati.
+				</p>
+				<button>Get for free</button>
+				<button>Learn more</button>
+			</div>
+		</section>
+	)
 }
