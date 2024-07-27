8995df4059744e9bd282bff61f5b3db8def44ffe
Viktor Rura <ruravictor@gmail.com>
2024-07-27 11:06:45 +0100

    Add some styles

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,16 +1,18 @@
 export const CallToAction = () => {
 	return (
-		<section>
+		<section className='bg-gradient-to-b from-white to-[#D2DCFF] py-24'>
 			<div className='container'>
-				<h2>Sign up fo free today</h2>
-				<p>
+				<h2 className='section-title'>Sign up fo free today</h2>
+				<p className='section-description'>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla rerum,
 					placeat esse architecto exercitationem voluptas labore debitis harum
 					accusamus minima suscipit magnam, atque iure! Tempore esse ut impedit
 					nostrum obcaecati.
 				</p>
-				<button>Get for free</button>
-				<button>Learn more</button>
+				<div>
+					<button className='btn btn-primary'>Get for free</button>
+					<button className='btn btn-text'>Learn more</button>
+				</div>
 			</div>
 		</section>
 	)
