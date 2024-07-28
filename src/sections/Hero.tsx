ab78ddb9a09754de442a86bb06209c5fa68b2073
Viktor Rura <ruravictor@gmail.com>
2024-07-28 08:28:22 +0100

    Adding animation to cylinder

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,11 +6,22 @@ import cogImage from '../assets/cog.png'
 import cylinder from '../assets/cylinder.png'
 import noodle from '../assets/noodle.png'
 
-import { motion } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
 
 export const Hero = () => {
+	const heroRef = useRef(null)
+	const { scrollYProgress } = useScroll({
+		target: heroRef,
+		offset: ['start end', 'end start'],
+	})
+	const translateY = useTransform(scrollYProgress, [0, 1], [150, -150])
+
 	return (
-		<section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom__left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
+		<section
+			ref={heroRef}
+			className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom__left,#183EC2,#EAEEFE_100%)] overflow-x-clip'
+		>
 			<div className='container'>
 				<div className='md:flex items-center'>
 					<div className='md:w-[478px]'>
@@ -48,12 +59,15 @@ export const Hero = () => {
 								ease: 'easeInOut',
 							}}
 						/>
-						<Image
-							src={cylinder}
+						<motion.img
+							src={cylinder.src}
 							alt='cylinder'
 							width={220}
 							height={220}
 							className='hidden md:block -top-8 -left-32 md:absolute'
+							style={{
+								translateY: translateY,
+							}}
 						/>
 						<Image
 							src={noodle}
