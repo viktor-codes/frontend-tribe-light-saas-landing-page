d417d6aadfe80fc6b61dff9c255e061d7926b0f2
Viktor Rura <ruravictor@gmail.com>
2024-07-28 08:20:39 +0100

    Fix duration for animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -44,7 +44,8 @@ export const Hero = () => {
 							transition={{
 								repeat: Infinity,
 								repeatType: 'mirror',
-								duration: 10,
+								duration: 3,
+								ease: 'easeInOut',
 							}}
 						/>
 						<Image
