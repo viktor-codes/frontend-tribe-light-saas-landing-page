961204bdd537f138290a8e9f588c9217205529a1
Viktor Rura <ruravictor@gmail.com>
2024-07-28 07:54:14 +0100

    Finish the footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,10 +10,16 @@ export const Footer = () => {
 	return (
 		<footer className='bg-black text-[#BCBCBC] text-sm py-10 text-center'>
 			<div className='container'>
-				<div className='inline-flex relative before:content-[""] before:h-full before:w-full before:absolute'>
-					<Image src={logo} alt='logo' width={40} height={40} />
+				<div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:h-full before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FFdd9B,#C2F0B1,#2FD8FE)]before:absolute">
+					<Image
+						src={logo}
+						alt='logo'
+						width={40}
+						height={40}
+						className='relative'
+					/>
 				</div>
-				<nav>
+				<nav className='flex flex-col gap-6 mt-6 md:flex-row md:justify-center'>
 					<a href=''>About</a>
 					<a href=''>Features</a>
 					<a href=''>Customers</a>
@@ -21,13 +27,14 @@ export const Footer = () => {
 					<a href=''>Help</a>
 					<a href=''>Careers</a>
 				</nav>
-				<div>
+				<div className='flex justify-center gap-6 mt-6'>
 					<SocialInsta />
 					<SocialLinkedIn />
 					<SocialPin />
 					<SocialX />
 					<SocialYouTube />
 				</div>
+				<p className='mt-6'>Copyrights </p>
 			</div>
 		</footer>
 	)
